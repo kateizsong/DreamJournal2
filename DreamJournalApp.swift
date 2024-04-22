738b6cf2d9b5275060a9e1a37995cb47660fe424
kateizsong <122414853+kateizsong@users.noreply.github.com>
2024-04-22 13:05:31 -0400

    Add files via upload

---
+++ DreamJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  DreamJournalApp.swift
+//  DreamJournal
+//
+//  Created by Soham Purushan on 4/10/24.
+//
+
+import SwiftUI
+
+@main
+struct DreamJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
